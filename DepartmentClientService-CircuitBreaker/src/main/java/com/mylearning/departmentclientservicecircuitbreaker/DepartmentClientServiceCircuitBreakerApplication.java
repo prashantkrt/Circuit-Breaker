158ed6067e8490e91356d860db871142baaf7b14
prashant <prashanttiwary10@gmail.com>
2025-03-27 23:07:01 +0530

    Initial Commit

---
+++ DepartmentClientService-CircuitBreaker/src/main/java/com/mylearning/departmentclientservicecircuitbreaker/DepartmentClientServiceCircuitBreakerApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.departmentclientservicecircuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentClientServiceCircuitBreakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepartmentClientServiceCircuitBreakerApplication.class, args);
+    }
+
+}
---
+++ DepartmentClientService-CircuitBreaker/src/test/java/com/mylearning/departmentclientservicecircuitbreaker/DepartmentClientServiceCircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.departmentclientservicecircuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentClientServiceCircuitBreakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
